480f566832bb5cecb04a4f35c271a64c5a51fd6c
Albin David C <105530284+albindavidc@users.noreply.github.com>
2024-12-14 16:39:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.ts
@@ -0,0 +1,8 @@
+function isHappy(n: number): boolean {
+    let finalResult = new Set<number>();
+
+    while(n !== 1 && !finalResult.has(n)){
+        finalResult.dd(n)
+        let result = 0;
+    }
+};
