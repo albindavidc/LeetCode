192d05b5e01c1e0b3fabfdb600b4be086dc9b6b8
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-05-12 11:28:40 +0530

    Time: 0 ms (100%), Space: 56.3 MB (90.37%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,27 +11,14 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let mergedList = new ListNode(0);
-    let curr = mergedList;
+    if(list1 === null) return list2;
+    if(list2 === null) return list1;
 
-    while(list1 !== null && list2 !== null){
-        if(list1.val <= list2.val){
-            curr.next = list1;
-            list1 = list1.next
-        }else{
-            curr.next = list2;
-            list2 = list2.next
-        }
-
-        curr = curr.next;
+    if(list1.val <= list2.val){
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    }else{
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2
     }
-
-    if(list1 !==null){
-        curr.next = list1;
-        
-    }else if(list2 !==null){
-        curr.next = list2;
-    }
-
-    return mergedList.next;
 };
