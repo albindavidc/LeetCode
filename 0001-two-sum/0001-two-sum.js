6a7079b84ce3639c4b74d3548be30840b243f9c7
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-05-13 09:15:19 +0530

    Time: 2 ms (70.59%), Space: 55.3 MB (38.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let newMap = new Map();
     for(let i =0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i,j];
-            }
+        const complement = target - nums[i]
+        if(newMap.has(complement)){
+            return [newMap.get(complement), i]
         }
+        newMap.set(nums[i], i)
     }
+    return []
 };
