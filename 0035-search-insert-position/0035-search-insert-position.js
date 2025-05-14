c4e0c22473d542e746711e2adc7ef04b5f1276a7
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-05-14 09:37:31 +0530

    Time: 0 ms (100%), Space: 55.7 MB (5.26%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while(left <= right){
-        let midd = Math.floor((left + right)/2);
-
-        if(nums[midd] === target){
-            return midd;
-        } else if(nums[midd] < target){
-            left = midd + 1;
-        } else{
-            right = midd - 1
+    for(let i =0; i<nums.length; i++){
+        if(nums[i] === target){
+            return i;
         }
+
+        if(nums[i] > target){
+            return i 
+        }
+
+        
     }
-    return left
+
+    return nums.length
+
+    
 };
