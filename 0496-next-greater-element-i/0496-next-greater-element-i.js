a26c29aa3703f752624d9afd0672016c8f9cd57f
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-06-13 18:17:58 +0530

    Time: 1 ms (97.65%), Space: 54.8 MB (95.24%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function (nums1, nums2) {
+    let ans = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+
+        let found = false;
+        let index = nums2.indexOf(nums1[i])
+
+        for (let j = index + 1; j < nums2.length; j++) {
+            if(nums2[j] > nums1[i]){
+                ans.push(nums2[j])
+                found = true;
+                break
+            }
+        }
+
+        if(!found){
+            ans.push(-1)
+        }
+    }
+
+    return ans
+
+    return ans
+};
