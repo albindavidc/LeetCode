0c3c377bcf74f437c68a60bac231cdf32c253421
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-07-14 08:28:16 +0530

    Time: 119 ms (12.35%), Space: 80.6 MB (11.4%) - LeetHub

---
+++ 2631-group-by/2631-group-by.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const result = {};
+
+    for(let item of this){
+        let key = fn(item);
+        if(!result.hasOwnProperty(key)){
+            result[key] = []
+        }
+        result[key].push(item)
+    }
+
+    return result
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
