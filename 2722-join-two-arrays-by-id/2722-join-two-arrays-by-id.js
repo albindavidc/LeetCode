52efa3443a0f9ccdba939a353193e949d01be310
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-07-15 08:30:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    let map = new Map();
+
+    for(let obj of arr1){
+        map.set(obj.id, {...obj})
+    }
+
+    for(let obj of arr2){
+        let existingObj = map.get(obj.id);
+        if(existingObj){
+            map.set(obj.id, {...existingObj, ...obj})
+        }else{
+            map.set(obj.id, {...obj})
+        }
+    }
+
+    let finalResult = Array.from(map.values()).sort((a,b) => a-b)
+    return finalResult
+
+};
