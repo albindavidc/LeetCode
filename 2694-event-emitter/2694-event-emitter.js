d428611d8e7ac76fde85e060c692669f94f5efe4
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-07-17 09:01:03 +0530

    Time: 42 ms (66.64%), Space: 53.4 MB (84.7%) - LeetHub

---
+++ 2694-event-emitter/2694-event-emitter.js
@@ -0,0 +1,56 @@
+class EventEmitter {
+    
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+
+    constructor(){
+        this.event = new Map();
+    }
+
+    subscribe(eventName, callback) {
+        if(!this.event.has(eventName)){
+            this.event.set(eventName, [])
+        }
+
+        let listener = this.event.get(eventName);
+        listener.push(callback);
+
+        return {
+            unsubscribe: () => {
+                let index = listener.indexOf(callback);
+                if(index !== -1){
+                    listener.splice(index, 1);
+                }
+            }
+        };
+    }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        if(!this.event.has(eventName)){
+            return [];
+        }
+
+        let listener = this.event.get(eventName);
+        return listener.map(callback => callback(...args))
+    }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
