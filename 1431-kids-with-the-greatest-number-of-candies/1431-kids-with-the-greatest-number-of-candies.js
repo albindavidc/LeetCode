54214c4d2e83a6a7090803fbc971032429f20d08
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-07-19 09:48:35 +0530

    Time: 0 ms (100%), Space: 55.5 MB (25.1%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -3,23 +3,7 @@
  * @param {number} extraCandies
  * @return {boolean[]}
  */
-var kidsWithCandies = function (candies, extraCandies) {
-    let result = [];
-
-    for (let i = 0; i < candies.length; i++) {
-        let temp = candies[i] + extraCandies
-        let bool = false;
-        for (let j = 0; j < candies.length; j++) {
-            if (temp >= candies[j]) {
-                bool = true;
-            } else {
-                bool = false;
-                break;
-            }
-        }
-        result.push(bool)
-
-
-    }
-    return result
+var kidsWithCandies = function(candies, extraCandies) {
+    let maxCandie = Math.max(...candies);
+    return candies.map(candie => candie+extraCandies >= maxCandie)
 };
