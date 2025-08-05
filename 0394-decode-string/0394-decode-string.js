04445a313e2c7366ce14f9ac0a1bfe8b766006aa
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-08-05 10:31:34 +0530

    Time: 0 ms (100%), Space: 52.9 MB (84.4%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -4,23 +4,23 @@
  */
 var decodeString = function(s) {
     let stack = [];
-    let currString = '';
+    let currChar = '';
     let currNum = 0;
 
     for(let char of s){
         if(!isNaN(char)){
-            currNum = currNum *10 + parseInt(char);
+            currNum = currNum * 10 + parseInt(char);
         }else if(char === '['){
-            stack.push([currString, currNum]);
-            currString = '';
+            stack.push([currChar, currNum]);
+            currChar = '';
             currNum = 0;
         }else if(char === ']'){
-            let [prevString, repeat] = stack.pop();
-            currString = prevString +  currString.repeat(repeat);
+            let [prevChar, repeat] = stack.pop();
+            currChar = prevChar + currChar.repeat(repeat);
         }else{
-            currString += char;
+            currChar += char
         }
     }
 
-    return currString
+    return currChar
 };
