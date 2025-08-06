f0ff066ee9d9ce4f63dc094245eec381d5b342d4
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-08-06 12:40:48 +0530

    Time: 2 ms (99.73%), Space: 98.6 MB (64.5%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -11,6 +11,7 @@
  */
 var deleteMiddle = function(head) {
     if(head === null || head.next === null) return null;
+
     let slow = head;
     let fast = head;
     let prev = null;
@@ -19,9 +20,10 @@ var deleteMiddle = function(head) {
         prev = slow;
         slow = slow.next;
         fast = fast.next.next;
+        
     }
 
-    prev.next = slow.next;
+    prev.next = slow.next
 
-    return head;
+    return head
 };
