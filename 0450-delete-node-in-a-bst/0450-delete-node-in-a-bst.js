132522872b13c51d8935c562906dbf66e9fbce35
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-08-11 08:50:18 +0530

    Time: 0 ms (100%), Space: 64.7 MB (83.86%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function (root, key) {
+    if (!root) return null;
+
+    if (key < root.val) {
+        root.left = deleteNode(root.left, key);
+    } else if (key > root.val) {
+        root.right = deleteNode(root.right, key);
+    } else {
+        if (!root.left && !root.right) {
+            return null;
+        }
+
+        if (!root.left) {
+            return root.right
+        }
+        if (!root.right) {
+            return root.left;
+        }
+
+        root.val = findMin(root.right).val
+        root.right = deleteNode(root.right, root.val)
+    }
+
+
+
+    return root
+};
+function findMin(root) {
+    if (!root) return null
+    if (!root.left) {
+        return root
+    }
+    return findMin(root.left)
+}
