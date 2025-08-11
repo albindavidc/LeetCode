3644c315b8ccf5667dbbacc279826f5eab54a458
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-08-11 10:46:49 +0530

    Time: 5 ms (39.95%), Space: 56.6 MB (73.77%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.js
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -8,23 +8,23 @@ var findCircleNum = function(isConnected) {
     let provinces = 0;
 
     for(let i = 0; i<isConnected.length; i++){
-
         if(!visited.has(i)){
             provinces++;
-            visited.add(i);
             queue.push(i);
+            visited.add(i);
 
             while(queue.length >0){
                 let node = queue.shift();
 
-                for(let j = 0; j< isConnected[i].length ; j++){
-                    if(isConnected[node][j] === 1 && !visited.has(j)){
-                        visited.add(j);
+                for(let j =0 ;j<isConnected[i].length; j++){
+                    if(!visited.has(j) && isConnected[node][j] === 1){
                         queue.push(j);
+                        visited.add(j)
                     }
                 }
             }
         }
     }
-        return provinces
+
+    return provinces
 };
