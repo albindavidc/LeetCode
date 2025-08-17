ff4cf19df2fdfd0722ebb83e062fcacf02f38c6d
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-08-17 13:47:20 +0530

    Time: 32 ms (52.58%), Space: 65.9 MB (71.82%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
@@ -6,27 +6,27 @@
 var nearestExit = function(maze, entrance) {
     const m = maze.length;
     const n = maze[0].length;
-    const [sr, sc] = entrance
-    const dir = [[-1, 0], [1,0], [0, -1], [0, 1]]
+    const [sr, sc] = entrance;
+    const dirs = [[-1, 0], [1,0], [0, -1], [0, 1]];
 
     maze[sr][sc] = '+';
 
-    let q = [[sr, sc, 0]];
+    let q  = [[sr, sc, 0]];
 
     while(q.length >0){
         const [r, c, d] = q.shift();
 
-        for(const [dr, dc] of dir){
+        for(const [dr, dc] of dirs){
             const nr = r + dr;
             const nc = c + dc;
 
-            if(nr >= 0 && nr < m && nc >= 0 && nc < n && maze[nr][nc] === '.'){
-                if(nr === 0 || nr === m - 1 || nc === 0 || nc === n - 1){
-                    return d + 1;
+            if(nr >=0 && nr < m && nc >= 0 && nc < n && maze[nr][nc] === '.'){
+                if(nr === 0 || nr === m-1 || nc === 0 || nc === n-1){
+                    return d+1;
                 }
-
+                
                 maze[nr][nc] = '+';
-                q.push([nr,nc, d+1])
+                q.push([nr, nc, d+1])
             }
         }
     }
