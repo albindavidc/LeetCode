c14cd756ce5707ccfbe8f6442ff7d2efe2cdef03
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-08-17 13:31:16 +0530

    Time: 17 ms (92.78%), Space: 66.2 MB (61.17%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} maze
+ * @param {number[]} entrance
+ * @return {number}
+ */
+var nearestExit = function(maze, entrance) {
+    const m = maze.length;
+    const n = maze[0].length;
+    const [sr, sc] = entrance
+    const dir = [[-1, 0], [1,0], [0, -1], [0, 1]]
+
+    maze[sr][sc] = '+';
+
+    let q = [[sr, sc, 0]];
+
+    while(q.length >0){
+        const [r, c, d] = q.shift();
+
+        for(const [dr, dc] of dir){
+            const nr = r + dr;
+            const nc = c + dc;
+
+            if(nr >= 0 && nr < m && nc >= 0 && nc < n && maze[nr][nc] === '.'){
+                if(nr === 0 || nr === m - 1 || nc === 0 || nc === n - 1){
+                    return d + 1;
+                }
+
+                maze[nr][nc] = '+';
+                q.push([nr,nc, d+1])
+            }
+        }
+    }
+
+    return -1
+};
