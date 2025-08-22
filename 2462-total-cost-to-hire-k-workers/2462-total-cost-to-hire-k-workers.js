45521e198600d3cc88f8e5cb3d100142d8bb14c1
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-08-22 13:27:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.js
@@ -0,0 +1,89 @@
+/**
+ * @param {number[]} costs
+ * @param {number} k
+ * @param {number} candidates
+ * @return {number}
+ */
+var totalCost = function(costs, k, candidates) {
+    
+    let heap = [];
+    let totalCost = 0;
+
+    function heapPush(cost, index){
+        heap.push([cost, index]);
+        heapifyUp();
+    }
+
+    function heapifyUp(){
+        let index = heap.length - 1;
+        while(index > 0){
+            let parent = Math.floor((index - 1) /2);
+            if(compare(heap[parent], heap[index])<0) break;
+
+            [heap[parent], heap[index]] = [heap[index], heap[parent]];
+            index = parent;
+        }
+    }
+
+    function heapPop(){
+        if(heap.length === 0) return null;
+        if(heap.length === 1) return heap.pop();
+        const removeVal = heap[0];
+        heap[0] = heap.pop();
+        heapifyDown()
+        return removeVal;
+    }
+
+    function heapifyDown(){
+        let index = 0;
+        while(true){
+            let smallest = index;
+            let left = 2 * index + 1;
+            let right = 2 * index + 2;
+
+            if(left < heap.length && compare(heap[left], heap[smallest]) <0 )smallest = left;
+            if(right < heap.length && compare(heap[right], heap[smallest]) < 0) smallest = right;
+
+            if(smallest === index) break;
+            [heap[smallest], heap[index]] = [heap[index], heap[smallest]];
+            index = smallest;
+        }
+    }
+
+    function compare(a,b){
+        if(a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0]
+    }
+
+    let left = 0;
+    let right = costs.length - 1;
+    for(let i = 0; i<candidates && left <= right; i++){
+        heapPush(costs[left], left)
+        left++
+    }
+    for(let i = 0; i<candidates && left <= right; i++){
+        heapPush(costs[right], right)
+        right--
+    }
+
+    for(let i = 0; i< k; i++){
+        
+        let popped = heapPop();
+        if(!popped) break;
+        let [cost, index] = popped
+        totalCost += cost;
+
+        if(left <= right){
+            if(index < left){
+                heapPush(costs[left++], left)
+                left++;
+            }else{
+                heapPush(costs[right], right)
+                right--
+            }
+        }
+
+    }
+
+    return totalCost
+};
