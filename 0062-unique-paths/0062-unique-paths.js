0e3e10d5b9c565030c95d7bc6b9650814f22d1fa
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-09-09 14:35:27 +0530

    Time: 0 ms (100%), Space: 53.5 MB (71.87%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let result = Array.from({length: m}, () => Array(n).fill(1))
+
+    for(let i = 1; i<m;i++){
+        for(let j = 1; j<n; j++){
+            result[i][j] = result[i-1][j] + result[i][j-1];
+        }
+    }
+
+    return result[m-1][n-1]
+};
