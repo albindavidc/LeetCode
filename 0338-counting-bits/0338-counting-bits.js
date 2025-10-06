43e3e1b1286313705ef12b778397b386354ae73a
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-10-06 08:54:02 +0530

    Time: 1 ms (90.59%), Space: 62.6 MB (37.05%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -2,20 +2,12 @@
  * @param {number} n
  * @return {number[]}
  */
-var countBits = function (n) {
-    let result = [0];
+var countBits = function(n) {
+    let result = Array(n+1).fill(0);
 
-    for (let i = 1; i <= n; i++) {
-        let binary = (i).toString(2)
-        let sum = 0;
-        for (let num = 0; num < binary.length; num++) {
-            if (binary[num] == 1) {
-                sum++;
-            }
-        }
-        result.push(sum);
+    for(let i = 1; i<=n; i++){
+        result[i] = result[i >> 1] + (i & 1);
     }
 
     return result
-
 };
