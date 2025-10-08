e3a831f54984a48df2b4548a09202eac3bc3a0ad
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-10-08 12:57:05 +0530

    Time: 0 ms (100%), Space: 55.7 MB (18.23%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.js
+++ 0520-detect-capital/0520-detect-capital.js
@@ -3,17 +3,10 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-    if(word === word.toUpperCase()){
-        return true;
-    }
-
-    if(word === word.toLowerCase()){
-        return true;
-    }
-
-    if(word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase()){
-        return true;
-    }
+    
+    if(word === word.toUpperCase()) return true;
+    if(word === word.toLowerCase()) return true;
+    if(word === word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase()) return true;
 
     return false;
 };
