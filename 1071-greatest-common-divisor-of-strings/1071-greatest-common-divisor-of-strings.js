4163bb9c992b98b7c5a8eeb65de21c0fb348fcd4
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-10-08 09:36:12 +0530

    Time: 0 ms (100%), Space: 56 MB (21.06%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -3,21 +3,11 @@
  * @param {string} str2
  * @return {string}
  */
-var gcdOfStrings = function(str1, str2) {
-    if(str1+str2 !== str2 +str1){
-        return '';
-    }
+var gcdOfStrings = function (str1, str2) {
+    if (str1 + str2 !== str2 + str1) return ""
 
-    const gcd = (a,b) => {
-        while(b !== 0){
-            let temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a
-    }
+    const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 
-    const len = gcd(str1.length, str2.length);
-
-    return str1.slice(0, len)
+    const gcdLen = gcd(str1.length, str2.length)
+    return str1.slice(0,gcdLen);
 };
