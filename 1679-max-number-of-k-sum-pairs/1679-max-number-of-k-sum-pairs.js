75a98fb4d5531a2ff1f78d26264fc601978d63a4
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-10-28 09:28:50 +0530

    Time: 142 ms (24.34%), Space: 66.5 MB (59.65%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -3,26 +3,23 @@
  * @param {number} k
  * @return {number}
  */
-var maxOperations = function (nums, k) {
-    let start = 0
-    let end = nums.length - 1
-    let resultCount = 0
+var maxOperations = function(nums, k) {
+    nums = nums.sort((a,b) => a-b);
+    let start = 0; end = nums.length -1;
+    let op = 0;
 
-    while (start < end) {
-        let sum = nums[start] + nums[end]
-        
-        if (sum === k) {
+    while (start < end){
+        let sum = nums[start] + nums[end];
+        if(sum===k){
+            op++
             start++;
             end--;
-            resultCount++;
-        }
-        else if (sum < k) {
+        }else if(sum < k){
             start++
-        } else {
-            end--;
+        }else{
+            end--
         }
-
     }
 
-    return resultCount
+    return op
 };
