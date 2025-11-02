1259d9ba98eb46060b47b95dca3a1446daa7ecc5
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-11-02 09:46:33 +0530

    Time: 41 ms (6.8%), Space: 63.1 MB (8.26%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -3,28 +3,42 @@
  * @param {number} k
  * @return {number}
  */
-var maxVowels = function(s, k) {
-    let vowels = ['a','e', 'i', 'o', 'u'];
-    let count = 0;
+var maxVowels = function (s, k) {
+    let start = '';
+    let maxNum = 0;
 
-    for(let i = 0; i<k; i++){
-        if(vowels.includes(s[i])){
-            count++;
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+
+
+    for (let i = 0; i < k; i++) {
+        start += s[i];
+        if (vowels.includes(s[i])) {
+            maxNum++;
+        }
+        if (maxNum === k) {
+            return k;
         }
     }
 
-    let maxCount = count;
+    let max = maxNum
 
-    for(let i = k;i<s.length; i++){
+    for (let i = k; i < s.length; i++) {
+        start += s[i] - s[i - k];
+        if (vowels.includes(s[i - k])) {
+            maxNum--;
+        }
+
         if(vowels.includes(s[i])){
-            count++
+            maxNum++
         }
-        if (vowels.includes(s[i-k])){
-            count--
-        }
-        maxCount = Math.max(maxCount, count);
 
-        if(maxCount === k) return maxCount
+        maxNum > max ? max = maxNum : null;
+
+        if(maxNum === k){
+            return k;
+        }
     }
-    return maxCount
+
+    return max
+
 };
