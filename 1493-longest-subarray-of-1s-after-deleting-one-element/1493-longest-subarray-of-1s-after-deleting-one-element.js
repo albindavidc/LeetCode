93fe8831249c6841f3b0dd3fe467d4f316a6d803
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-11-10 09:00:25 +0530

    Time: 2 ms (62.29%), Space: 61.5 MB (29.92%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -5,26 +5,18 @@
 var longestSubarray = function(nums) {
     let left = 0;
     let zeroCount = 0;
-    let longest = 0;
+    let maxLen = 0;
 
     for(let right = 0; right < nums.length; right++){
-        if(nums[right] === 0){
-            zeroCount++;
-        }
+        if(nums[right] === 0) zeroCount++;
 
-        while(zeroCount > 1){
-            if(nums[left] === 0){
-                zeroCount--;
-            }
+        while(zeroCount >1){
+            if(nums[left] === 0) zeroCount--;
             left++;
         }
 
-        longest = Math.max(longest, right - left);
+        maxLen = Math.max(maxLen, right - left)
     }
 
-    if(longest === nums.length){
-        return longest - 1;
-    }
-
-    return longest
+    return maxLen
 };
