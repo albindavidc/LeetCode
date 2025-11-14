b173318404676ff1d39fba3a9455d7794d4d9f37
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-11-14 09:11:28 +0530

    Time: 61 ms (9.04%), Space: 59.8 MB (91.17%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -3,23 +3,26 @@
  * @param {number[]} nums2
  * @return {number[][]}
  */
-var findDifference = function(nums1, nums2) {
-    let result1 = [];
+var findDifference = function (nums1, nums2) {
+    let result = [[], []];
 
-    for(let i = 0; i<nums1.length; i++){
+    for (let i = 0; i < nums1.length; i++) {
+        if (!result[0].includes(nums1[i]) && !nums2.includes(nums1[i])) {
 
-        if(!nums2.includes(nums1[i]) && !result1.includes(nums1[i])){
-            result1.push(nums1[i])
+            result[0].push(nums1[i])
+
         }
+
     }
 
-    let result2 = [];
+    for (let i = 0; i < nums2.length; i++) {
 
-    for(let i = 0; i<nums2.length; i++){
-        if(!nums1.includes(nums2[i]) && !result2.includes(nums2[i])){
-            result2.push(nums2[i])
+        if (!result[1].includes(nums2[i]) && !nums1.includes(nums2[i])) {
+            result[1].push(nums2[i])
         }
+
+
     }
 
-    return [result1, result2]
+    return result
 };
