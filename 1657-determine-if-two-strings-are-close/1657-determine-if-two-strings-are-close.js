c3ebba3bf814e0e62825aabeb3d85fd87ff16db0
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-11-17 09:42:27 +0530

    Time: 61 ms (79.86%), Space: 63.8 MB (44.31%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -6,38 +6,25 @@
 var closeStrings = function(word1, word2) {
     if(word1.length !== word2.length) return false;
 
-    let freq1 = new Array(26).fill(0);
-    let freq2 = new Array(26).fill(0);
+    let freq1 = new Map();
+    let freq2 = new Map();
 
-    let char1 = new Array(26).fill(false);
-    let char2 = new Array(26).fill(false);
+    for(let i = 0; i<word1.length; i++){
+        freq1.set(word1[i], (freq1.get(word1[i]) || 0) + 1);
+        freq2.set(word2[i], (freq2.get(word2[i]) || 0) + 1);
 
-    for(let char of word1){
-        let inx = char.charCodeAt() - 97
-        freq1[inx]++
-        char1[inx] = true;
     }
 
-    for(let char of word2){
-        let inx = char.charCodeAt() - 97;
-        freq2[inx]++;
-        char2[inx] = true;
-    };
-
-    for(let i = 0; i <26; i++){
-        if(char1[i] !== char2[i]){
-            return false;
-        }
+    for(let char of freq1.keys()){
+        if(!freq2.has(char)) return false;
     }
 
-    freq1.sort((a,b) => a-b);
-    freq2.sort((a,b) => a-b);
+    let count1 = Array.from(freq1.values()).sort()
+    let count2 = Array.from(freq2.values()).sort()
 
-    for(let i  = 0; i<26; i++){
-        if(freq1[i] !== freq2[i]){
-            return false;
-        }
+    for(let i=0; i<count1.length; i++){
+        if(count1[i] !== count2[i]) return false;
     }
 
-    return true;
+    return true
 };
