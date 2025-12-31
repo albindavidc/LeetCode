db3da752610dda278c004947f5cc77664d1324fe
Albin David C <105530284+albindavidc@users.noreply.github.com>
2025-12-31 10:07:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b)
+
+    let content = 0;
+
+    let i = 0;
+    let j = 0;
+
+    while (i < g.length) {
+        if (g[i] <= s[j]) {
+            content++
+            i++;
+        }
+
+        j++;
+    }
+
+    return content
+};
